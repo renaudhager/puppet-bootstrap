5dab4eb4c475d6a1e2b89b327a71f1ffc8eb7a06
Renaud Hager <renaud.hager@gmail.com>
2016-09-15 20:44:05 +0100

    Install git. Exec git clone.

--- node.pp
+++ node.pp
@@ -1,6 +1,20 @@
+# Boostrap node with puppet
+
+# Setup hiera file
 file { '/etc/puppetlabs/code/hiera.yaml':
   owner  => 'root',
   group  => 'root',
   mode   => '0644',
   source => 'file:///tmp/puppet-bootstrap/files/hiera.yaml',
+}
+
+# Install git
+package { 'git':
+  ensure => 'present',
+}
+
+# Git clone of puppet code
+exec { 'gitclone':
+  command => 'git clone https://github.com/renaudhager/puppet-masterless.git /etc/puppetlabs/code/environments/production',
+  require => Package['git'],
 }
