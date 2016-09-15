f8ebc13e8ca428c9d22b434e88bb8b7d733dbb05
Renaud Hager <renaud.hager@gmail.com>
2016-09-15 23:41:27 +0100

    Use r10k to deploy puppet code.

--- node.pp
+++ node.pp
@@ -8,20 +8,59 @@ file { '/etc/puppetlabs/code/hiera.yaml':
   source => 'file:///tmp/puppet-bootstrap/files/hiera.yaml',
 }
 
-# Install git
-package { 'git':
-  ensure => 'present',
+# Install r10k
+file { '/etc/puppetlabs/r10k':
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+file { '/etc/puppetlabs/r10k/r10k.yaml':
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  source  => 'file:///tmp/puppet-bootstrap/files/r10k.yaml',
+  require => File['/etc/puppetlabs/r10k'],
+}
+
+exec{ 'r10k':
+  command => '/opt/puppetlabs/puppet/bin/r10k deploy environment --puppetfile',
+  require => [
+    Exec['rmproduction'],
+    File['/etc/puppetlabs/r10k/r10k.yaml']
+  ]
 }
 
 # Remove production original dir
 exec { 'rmproduction':
   command => '/bin/rm -rf /etc/puppetlabs/code/environments/production',
 }
+
+# # Install git
+# package { 'git':
+#   ensure => 'present',
+# }
+
+# package { 'librarian-puppet':
+#   ensure   => 'present',
+#   provider => 'gem',
+# }
+
+
+
+
+
 # Git clone of puppet code
-exec { 'gitclone':
-  command => '/usr/bin/git clone https://github.com/renaudhager/puppet-masterless.git /etc/puppetlabs/code/environments/production',
-  require => [
-    Package['git'],
-    Exec['rmproduction'],
-  ],
-}
+# exec { 'gitclone':
+#   command => '/usr/bin/git clone https://github.com/renaudhager/puppet-masterless.git /etc/puppetlabs/code/environments/production',
+#   require => [
+#     Package['git'],
+#     Exec['rmproduction'],
+#   ],
+# }
+
+# exec { 'installmodule':
+#   command => '/usr/local/bin/librarian-puppet install',
+#   requied => Exec['gitclone'],
+#
+# }
