2e595eda067249f84955544bc3ca1e39551b7597
Renaud Hager <renaud.hager@gmail.com>
2017-02-08 19:01:23 +0000

    Add r10k gem installation during bootstrap.

--- node.pp
+++ node.pp
@@ -23,10 +23,14 @@ file { '/etc/puppetlabs/r10k/r10k.yaml':
   require => File['/etc/puppetlabs/r10k'],
 }
 
+exec {'r10k_installation':
+  command => '/opt/puppetlabs/puppet/bin/gem install r10k',
+}
+
 exec{ 'r10k':
   command => '/opt/puppetlabs/puppet/bin/r10k deploy environment --puppetfile',
   require => [
-    Exec['rmproduction'],
+    Exec['rmproduction', 'r10k_installation'],
     File['/etc/puppetlabs/r10k/r10k.yaml']
   ]
 }
